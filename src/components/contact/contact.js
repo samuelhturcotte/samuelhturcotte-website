24d34a325a9a168c8c23beba6dee707ca221b3ac
Samuel Turcotte <samuelhturcotte@gmail.com>
2022-11-08 20:12:56 -0500

    Updated Form

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -16,24 +16,24 @@ function BasicExample() {
 
                 <h2 className="form-title">Let's Connect</h2>
 
-                <Form.Group className="section-container" controlId="formBasicEmail" >
-                    <Form.Label className="section-label" >Name</Form.Label>
-                    <Form.Control className="section-input" type="name" name="name" placeholder="Enter your name" />
+                <Form.Group className="section-container" controlId="formBasicEmail">
+                    <Form.Label className="section-label" for="name">Name</Form.Label>
+                    <Form.Control className="section-input" id="name" type="text" name="name" placeholder="Enter your name" required/>
 
                 </Form.Group>
 
                 <Form.Group className="section-container" controlId="formBasicPassword">
-                    <Form.Label className="section-label">Email</Form.Label>
-                    <Form.Control className="section-input" type="email" name="email" placeholder="Enter your email" />
+                    <Form.Label className="section-label" for="email">Email</Form.Label>
+                    <Form.Control className="section-input" id="email" type="email" name="email" placeholder="Enter your email" required />
                 </Form.Group>
 
                 <Form.Group className="section-container" controlId="exampleForm.ControlTextarea1">
-                    <Form.Label className="section-label">Message</Form.Label>
-                    <Form.Control className="section-message" name="message" placeholder="Write your message" as="textarea" rows={4} />
+                    <Form.Label className="section-label" for="message">Message</Form.Label>
+                    <Form.Control className="section-message" id="message" name="message" placeholder="Write your message" as="textarea" rows={4} required />
                 </Form.Group>
 
 
-                <Button className="contact-button" variant="primary" type="submit">
+                <Button className="contact-button" variant="primary" name="submit" type="submit">
                     Submit
                 </Button>
             </Form>
