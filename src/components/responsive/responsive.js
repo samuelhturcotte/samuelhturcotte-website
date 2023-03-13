19bae849de40a033bb8141901912561456270265
Samuel Turcotte <samuelhturcotte@gmail.com>
2023-03-13 04:09:58 -0400

    Updated Responsive Menu Copy

--- src/components/responsive/responsive.js
+++ src/components/responsive/responsive.js
@@ -14,7 +14,7 @@ function Responsive() {
 
             <ul className="responsive-container-menu">
                 <li><Link className="menu-items" to="/">ABOUT</Link></li>
-                <li><Link className="menu-items" to="/projects"> UX PROJECTS</Link></li>
+                <li><Link className="menu-items" to="/projects">PROJECTS</Link></li>
                 <li><Link className="menu-items" to="/contact">CONTACT</Link></li>
                 <li className="menu-items"><a className="responsive-menu-icons" href="https://github.com/samuelhturcotte" target="_blank" rel="noreferrer" ><Github /></a></li>
                 <li className="menu-items"><a className="responsive-menu-icons" href="https://linkedin.com/in/samuelhturcotte" target="_blank" rel="noreferrer" ><Linkedin /></a></li>
